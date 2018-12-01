fbbb9e288576636b726a809a5156d69f622f8f8e
Haley Kell <haley.a.kell@gmail.com>
2018-12-01 00:20:54 -0600

    no code yet, just ideas and plans

--- src/main/java/com/vcam123/devathlon/DevAthlon.java
+++ src/main/java/com/vcam123/devathlon/DevAthlon.java
@@ -6,7 +6,28 @@ public class DevAthlon extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // MY IDEAS ONLY: NO ONE STEAL, I'LL KNOW IF YOU DO, LOVE YOU ALL
+        // ALSO DON'T JUDGE ME IT'S A WORK IN PROGRESS I HAVE RESEARCH TO DO SHHHHH
+            // Two way mirrors
+                // right click then say a person's ign while holding a glass pane item with specific lore
+                // If they're online AND they have a glass pane with lore anywhere in their inventory
+                // send them a message saying "ign would like to talk! hold your mirror to talk!"
+                // then they can send messages to each other
+            // Flying carpets
+                // Hold a carpet in one hand, fence in the other
+                // decide on controls
 
+        // HERE ARE MY PROBLEMS AND STUFF TO LOOK INTO:
+            // textures?
+            // runnables and listeners?
+                // right click listener then check what item?
+            // may need to adjust mirrors to make sure you agree to message or a better way of accepting
+            // carpet controls
+
+        // YAY LETS GET PREPARED! HERE ARE MY NEXT STEPS TO TAKE:
+            // Set up custom items
+                // Display name, lore
+                // maybe textures
     }
 
     @Override
