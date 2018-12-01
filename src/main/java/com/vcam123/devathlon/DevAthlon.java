2534fd7183d40b10114b88cbcd618dfd7479e58f
Bjarne Koll <LynxPlay101@gmail.com>
2018-12-01 03:35:39 +0100

    Initial Commit

---
+++ src/main/java/com/vcam123/devathlon/DevAthlon.java
@@ -0,0 +1,16 @@
+package com.vcam123.devathlon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DevAthlon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
