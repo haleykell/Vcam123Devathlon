2058ecc4543f4d588b60dd7df0ae89d2e1727c38
Haley Kell <haley.a.kell@gmail.com>
2018-12-03 12:14:40 -0600

    Made events code prettier

--- src/main/java/com/vcam123/devathlon/events/MirrorEvents.java
+++ src/main/java/com/vcam123/devathlon/events/MirrorEvents.java
@@ -17,8 +17,10 @@ import java.util.UUID;
 
 public class MirrorEvents implements Listener {
 
-    private MirrorMessage messages;
-    private static Plugin plugin = DevAthlon.getPlugin(DevAthlon.class);
+    // TODO: Cancelling communication after a certain amount of time?
+
+    private static MirrorMessage messages = new MirrorMessage();
+    private Plugin plugin = DevAthlon.getPlugin(DevAthlon.class);
 
     @EventHandler
     public void onRightClick(PlayerInteractEvent event) {
@@ -30,95 +32,110 @@ public class MirrorEvents implements Listener {
         if (!action.equals(Action.RIGHT_CLICK_AIR)) return;
         if (!event.hasItem() || (event.hasItem() && !event.getItem().equals(mirror.getMirror()))) return;
 
-        messages = new MirrorMessage();
-        // Maybe save this to a config instead
-        plugin.getConfig().set("Users." + player.getUniqueId().toString() + ".Messages.active", true);
-        plugin.getConfig().set("Users." + player.getUniqueId().toString() + ".Messages.initiated", player.getUniqueId().toString());
-        // This will be who they are messaging
-        plugin.getConfig().set("Users." + player.getUniqueId().toString() + ".Messages.receiving", "no one yet");
+        messages.setActive(player, true);
+        messages.setInitiated(player, player);
+        messages.setReceiving(player, null);
         plugin.saveConfig();
-        player.sendMessage(ChatColor.BLUE + "Who would you like to talk to using the two way mirror?");
+
+        player.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                ChatColor.BLUE + "Who would you like to talk to using the two way mirror?");
     }
 
-    // I KNOW THIS IS SUPER FUCKING UGLY MAYBE I'LL HAVE TIME TO DETERMINE AN EASIER WAY TO DO IT
+    // OKAY I MADE IT A LITTLE LESS UGLY BUT ITS STILL KINDA UGLY OH WELL
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
-        event.setCancelled(true);
         Player player = event.getPlayer();
+        if (!messages.isActive(player) && messages.getInitiated(player) == null) return;
+        event.setCancelled(true);
         String message = event.getMessage();
-        if (plugin.getConfig().get("Users." + player.getUniqueId() + ".Messages.active").equals(true)
-                && ((String) plugin.getConfig().get("Users." + player.getUniqueId().toString() + ".Messages.initiated")).equalsIgnoreCase(player.getUniqueId().toString())
-                && ((String)plugin.getConfig().get("Users." + player.getUniqueId().toString() + ".Messages.receiving")).equalsIgnoreCase("no one yet")) {
+
+        Player receiving = messages.getReceiving(player);
+        String receivingUUID;
+        if (receiving == null) receivingUUID = "no one yet";
+        else receivingUUID = receiving.getUniqueId().toString();
+
+        if ( messages.getInitiated(player).getUniqueId().toString().equalsIgnoreCase(player.getUniqueId().toString())
+                && messages.isActive(player)&& receivingUUID.equalsIgnoreCase("no one yet")) {
             TwoWayMirror mirror = new TwoWayMirror();
-            Player receiving = Bukkit.getPlayer(message);
+
+            receiving = Bukkit.getPlayer(message);
             if (receiving == null) {
                 setDefaultConfig(player);
                 plugin.saveConfig();
-                player.sendMessage(ChatColor.BLUE + "Player is not online or doesn't exist.");
+                player.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                        ChatColor.BLUE + "Player is not online or doesn't exist.");
                 return;
             }
+
+            receiving.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                    ChatColor.BLUE + player.getPlayerListName() + "would like to message you.");
             if (!receiving.getInventory().contains(mirror.getMirror())) {
-                receiving.sendMessage(ChatColor.BLUE + "You need to be holding a two way mirror!");
+                receiving.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                        ChatColor.BLUE + "You need to be holding a two way mirror!");
+                player.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " + ChatColor.BLUE +
+                        receiving.getPlayerListName() + "was not holding a two way mirror! Try again.");
                 setDefaultConfig(player);
                 plugin.saveConfig();
                 return;
             }
-            plugin.getConfig().set("Users." + player.getUniqueId().toString() + ".Messages.receiving", receiving.getUniqueId().toString());
-            plugin.getConfig().set("Users." + receiving.getUniqueId().toString() + ".Messages.receiving", player.getUniqueId().toString());
-            plugin.getConfig().set("Users." + receiving.getUniqueId().toString() + ".Messages.initiated", player.getUniqueId().toString());
-            receiving.sendMessage(ChatColor.BLUE + player.getPlayerListName() + "would like to message you.");
-            receiving.sendMessage(ChatColor.BLUE + "To confirm messaging, say the other person's name in chat.");
+
+            messages.setReceiving(player, receiving);
+            messages.setInitiated(receiving, player);
+            messages.setReceiving(receiving, player);
             plugin.saveConfig();
+
+            receiving.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                    ChatColor.BLUE + "To confirm messaging, say the other person's name in chat.");
         }
-        else if (plugin.getConfig().get("Users." + player.getUniqueId().toString() + ".Messages.active").equals(false)) {
+        else if (!messages.isActive(player)) {
             if (message.equalsIgnoreCase("no")) {
-                UUID initiated = UUID.fromString((String) plugin.getConfig().get("Users." + player.getUniqueId().toString() + ".Messages.initiated"));
+                UUID initiated = UUID.fromString(messages.getInitiated(player).getUniqueId().toString());
                 Player init = Bukkit.getPlayer(initiated);
                 setDefaultConfig(player);
-                init.sendMessage(ChatColor.BLUE + "Player denied communication.");
+                init.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                        ChatColor.BLUE + "Player denied communication.");
                 setDefaultConfig(init);
                 plugin.saveConfig();
             }
             else if (Bukkit.getOnlinePlayers().contains(Bukkit.getPlayer(message))){
-                plugin.getConfig().set("Users." + player.getUniqueId().toString() + ".Messages.active", true);
+                messages.setActive(player, true);
                 Player initiated = Bukkit.getPlayer(message);
-                player.sendMessage(ChatColor.BLUE + "Communication has started.\n");
-                initiated.sendMessage(ChatColor.BLUE + "Player has accepted. Communication has started.\n");
+                player.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                        ChatColor.BLUE + "Communication has started.\n");
+                initiated.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                        ChatColor.BLUE + "Player has accepted. Communication has started.\n");
             }
             else {
-                player.sendMessage(ChatColor.BLUE + "Something went wrong. Cancelling communication.");
-                UUID initiated = UUID.fromString((String) plugin.getConfig().get("Users." + player.getUniqueId().toString() + ".Messages.initiated"));
+                player.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                        ChatColor.BLUE + "Something went wrong. Cancelling communication.");
+                UUID initiated = UUID.fromString(messages.getInitiated(player).getUniqueId().toString());
                 Player init = Bukkit.getPlayer(initiated);
                 setDefaultConfig(player);
-                init.sendMessage(ChatColor.BLUE + "Something went wrong. Cancelling communication.");
+                init.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                        ChatColor.BLUE + "Something went wrong. Cancelling communication.");
                 setDefaultConfig(init);
                 plugin.saveConfig();
             }
         }
         else {
-            plugin.getServer().getConsoleSender().sendMessage(ChatColor.RED + "\ntest4\n");
             if (message.equalsIgnoreCase("goodbye")) {
                 setDefaultConfig(player);
-                player.sendMessage(ChatColor.BLUE + "Communication ending.");
-                // TODO: FIX THIS
-                // This should check the other person and reset their config and send them the communication ending message
-                // Also need this to send regular messages back and forth
-                // How to get Player from UUID cause this doesn't seem to be working correctly
-                UUID other = UUID.fromString((String) plugin.getConfig().get("Users." + player.getUniqueId().toString() + ".Messages.initiated"));
-                if (((String) plugin.getConfig().get("Users." + player.getUniqueId().toString() + ".Messages.initiated")).equalsIgnoreCase(player.getUniqueId().toString())) {
-                    other = UUID.fromString((String) plugin.getConfig().get("Users." + player.getUniqueId().toString() + ".Messages.receiving"));
-                }
-                Player player2 = Bukkit.getPlayer(other);
-                player2.sendMessage(ChatColor.BLUE + "Communication ending.");
-                setDefaultConfig(player2);
+                player.sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                        ChatColor.BLUE + "You said goodbye. Communication ending.");
+                messages.getReceiving(player).sendMessage(ChatColor.LIGHT_PURPLE + "MIRROR: " +
+                        ChatColor.BLUE + player.getPlayerListName() + "said goodbye. Communication ending.");
+                setDefaultConfig(messages.getReceiving(player));
                 plugin.saveConfig();
+            }
+            else {
+                messages.sendMessage(player, message);
             }
         }
     }
 
     public static void setDefaultConfig(Player player) {
-        plugin.getConfig().set("Users." + player.getUniqueId().toString() + ".Messages.active", false);
-        plugin.getConfig().set("Users." + player.getUniqueId().toString() + ".Messages.initiated", "no one yet");
-        plugin.getConfig().set("Users." + player.getUniqueId().toString() + ".Messages.receiving", "no one yet");
+        messages.setActive(player, false);
+        messages.setReceiving(player, null);
+        messages.setInitiated(player, null);
     }
 }
